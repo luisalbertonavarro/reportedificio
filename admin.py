112cf5f6c5556bbc6738ca335b17a2620b24399d
Luis Navarro <luis_navarro@mba.berkeley.edu>
2015-01-13 21:17:32 -0800

    Moved functionality to add table entries to admin page. Also cleaned up directory structure.

--- admin.py
+++ admin.py
@@ -33,7 +33,7 @@ class UserUpload(db.Model):
     payment_date = db.StringProperty()
     blob = blobstore.BlobReferenceProperty()
 
-class MainPage(webapp2.RequestHandler):
+class AdminPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         login_url = users.create_login_url(self.request.path)
@@ -44,8 +44,8 @@ class MainPage(webapp2.RequestHandler):
         q.ancestor(db.Key.from_path('UserUpload', 'UserUploadGroup'))
         uploads = q.fetch(100)
 
-        upload_url = blobstore.create_upload_url('/upload')
-        template = template_env.get_template('templates/home.htm')
+        upload_url = blobstore.create_upload_url('/admin/upload')
+        template = template_env.get_template('templates/admin.htm')
         context = {
             'user': user,
             'login_url': login_url,
@@ -55,6 +55,27 @@ class MainPage(webapp2.RequestHandler):
         }
 
         self.response.write(template.render(context))
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        user = users.get_current_user()
+        emission_date = self.request.params['emission_date']
+        name = self.request.params['name']
+        description = self.request.params['description']
+        amount = self.request.params['amount']
+        payment_date = self.request.params['payment_date']
+
+        for blob_info in self.get_uploads('upload'):
+            upload = UserUpload(parent=db.Key.from_path('UserUpload', 'UserUploadGroup'),
+                                user=user,
+                                emission_date=emission_date,
+                				name=name,
+                                description=description,
+				                amount=amount,
+				                payment_date=payment_date,
+                                blob=blob_info.key())
+            upload.put()
+        self.redirect('/admin/main')
 
 class ViewHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self):
@@ -70,8 +91,21 @@ class ViewHandler(blobstore_handlers.BlobstoreDownloadHandler):
 
         self.send_blob(upload.blob)
 
+class DeleteHandler(webapp2.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        entities_to_delete = []
+        for delete_key in self.request.params.getall('delete'):
+          upload = db.get(delete_key)
+          entities_to_delete.append(upload.key())
+          entities_to_delete.append(db.Key.from_path('UserUpload',
+                                    'UserUploadGroup'))
+          db.delete(entities_to_delete)
+        self.redirect('/admin/main')
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/view', ViewHandler),
+    ('/admin/main', AdminPage),
+    ('/admin/upload', UploadHandler),
+    ('/admin/view', ViewHandler),
+    ('/admin/delete', DeleteHandler)
 ], debug=True)
